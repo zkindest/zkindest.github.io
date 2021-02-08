60e14c857115bfe5bb1bec2ceb61af7b0fe93f49
afroz1198 <mohammedafroz.1198@gmail.com>
2021-02-08 17:51:08 +0530

    Deploy Next.js to gh-pages

---
+++ _next/static/webpack/1.2975d9b805d5c66b1e53.hot-update.js
@@ -0,0 +1,29 @@
+webpackHotUpdate_N_E(1,{
+
+/***/ "./components/Recommend.js":
+false,
+
+/***/ "./node_modules/@babel/runtime/helpers/esm/extends.js":
+false,
+
+/***/ "./node_modules/@babel/runtime/helpers/esm/objectWithoutProperties.js":
+false,
+
+/***/ "./node_modules/@babel/runtime/helpers/esm/objectWithoutPropertiesLoose.js":
+false,
+
+/***/ "./node_modules/@mdx-js/react/dist/esm.js":
+false,
+
+/***/ "./posts/hello.mdx":
+/*!*************************!*\
+  !*** ./posts/hello.mdx ***!
+  \*************************/
+/*! exports provided: frontMatter, default */
+/***/ (function(module, exports) {
+
+eval("throw new Error(\"Module build failed (from ./node_modules/@mdx-js/loader/index.js):\\nSyntaxError: /home/afroz1198/blog/posts/hello.mdx: Unexpected token (2:0)\\n\\n\\u001b[0m \\u001b[90m 1 | \\u001b[39m\\u001b[36mimport\\u001b[39m \\u001b[33mRecommend\\u001b[39m from \\u001b[32m'@/components/Recommend.js'\\u001b[39m\\u001b[33m;\\u001b[39m\\u001b[0m\\n\\u001b[0m\\u001b[31m\\u001b[1m>\\u001b[22m\\u001b[39m\\u001b[90m 2 | \\u001b[39m\\u001b[33m#\\u001b[39m sadasd\\u001b[0m\\n\\u001b[0m \\u001b[90m   | \\u001b[39m\\u001b[31m\\u001b[1m^\\u001b[22m\\u001b[39m\\u001b[0m\\n    at Object._raise (/home/afroz1198/blog/node_modules/@babel/parser/lib/index.js:799:17)\\n    at Object.raiseWithData (/home/afroz1198/blog/node_modules/@babel/parser/lib/index.js:792:17)\\n    at Object.raise (/home/afroz1198/blog/node_modules/@babel/parser/lib/index.js:786:17)\\n    at Object.unexpected (/home/afroz1198/blog/node_modules/@babel/parser/lib/index.js:9089:16)\\n    at Object.parseExprAtom (/home/afroz1198/blog/node_modules/@babel/parser/lib/index.js:10416:20)\\n    at Object.parseExprAtom (/home/afroz1198/blog/node_modules/@babel/parser/lib/index.js:4748:20)\\n    at Object.parseExprSubscripts (/home/afroz1198/blog/node_modules/@babel/parser/lib/index.js:9976:23)\\n    at Object.parseUpdate (/home/afroz1198/blog/node_modules/@babel/parser/lib/index.js:9956:21)\\n    at Object.parseMaybeUnary (/home/afroz1198/blog/node_modules/@babel/parser/lib/index.js:9945:17)\\n    at Object.parseExprOps (/home/afroz1198/blog/node_modules/@babel/parser/lib/index.js:9815:23)\\n    at Object.parseMaybeConditional (/home/afroz1198/blog/node_modules/@babel/parser/lib/index.js:9789:23)\\n    at Object.parseMaybeAssign (/home/afroz1198/blog/node_modules/@babel/parser/lib/index.js:9752:21)\\n    at Object.parseExpressionBase (/home/afroz1198/blog/node_modules/@babel/parser/lib/index.js:9696:23)\\n    at /home/afroz1198/blog/node_modules/@babel/parser/lib/index.js:9690:39\\n    at Object.allowInAnd (/home/afroz1198/blog/node_modules/@babel/parser/lib/index.js:11442:16)\\n    at Object.parseExpression (/home/afroz1198/blog/node_modules/@babel/parser/lib/index.js:9690:17)\");//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiIuL3Bvc3RzL2hlbGxvLm1keC5qcyIsInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./posts/hello.mdx\n");
+
+/***/ })
+
+})
---
+++ _next/static/webpack/2.a583cd6ace20de4f9e7e.hot-update.js
@@ -0,0 +1,32 @@
+webpackHotUpdate_N_E(2,{
+
+/***/ "./components/Recommend.js":
+false,
+
+/***/ "./components/button.js":
+false,
+
+/***/ "./node_modules/@babel/runtime/helpers/esm/extends.js":
+false,
+
+/***/ "./node_modules/@babel/runtime/helpers/esm/objectWithoutProperties.js":
+false,
+
+/***/ "./node_modules/@babel/runtime/helpers/esm/objectWithoutPropertiesLoose.js":
+false,
+
+/***/ "./node_modules/@mdx-js/react/dist/esm.js":
+false,
+
+/***/ "./posts/fifth-post.mdx":
+/*!******************************!*\
+  !*** ./posts/fifth-post.mdx ***!
+  \******************************/
+/*! exports provided: frontMatter, default */
+/***/ (function(module, exports) {
+
+eval("throw new Error(\"Module build failed (from ./node_modules/@mdx-js/loader/index.js):\\nSyntaxError: /home/afroz1198/blog/posts/fifth-post.mdx: Adjacent JSX elements must be wrapped in an enclosing tag. Did you want a JSX fragment <>...</>? (3:0)\\n\\n\\u001b[0m \\u001b[90m 1 | \\u001b[39m\\u001b[36mimport\\u001b[39m \\u001b[33mButton\\u001b[39m from \\u001b[32m'@/components/button.js'\\u001b[39m\\u001b[33m;\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 2 | \\u001b[39m\\u001b[33m<\\u001b[39m\\u001b[33mButton\\u001b[39m\\u001b[33m>\\u001b[39m👋 \\u001b[33mHello\\u001b[39m\\u001b[33m<\\u001b[39m\\u001b[33m/\\u001b[39m\\u001b[33mButton\\u001b[39m\\u001b[33m>\\u001b[39m\\u001b[0m\\n\\u001b[0m\\u001b[31m\\u001b[1m>\\u001b[22m\\u001b[39m\\u001b[90m 3 | \\u001b[39m\\u001b[33m<\\u001b[39m\\u001b[33mRecommend\\u001b[39m links\\u001b[33m=\\u001b[39m{[{\\u001b[0m\\n\\u001b[0m \\u001b[90m   | \\u001b[39m\\u001b[31m\\u001b[1m^\\u001b[22m\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 4 | \\u001b[39m  name\\u001b[33m:\\u001b[39m \\u001b[32m'sdf'\\u001b[39m\\u001b[33m,\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 5 | \\u001b[39m  slug\\u001b[33m:\\u001b[39m \\u001b[32m'sdf'\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 6 | \\u001b[39m}]}\\u001b[33m>\\u001b[39m\\u001b[33m<\\u001b[39m\\u001b[33m/\\u001b[39m\\u001b[33mRecommend\\u001b[39m\\u001b[33m>\\u001b[39m\\u001b[0m\\n    at Object._raise (/home/afroz1198/blog/node_modules/@babel/parser/lib/index.js:799:17)\\n    at Object.raiseWithData (/home/afroz1198/blog/node_modules/@babel/parser/lib/index.js:792:17)\\n    at Object.raise (/home/afroz1198/blog/node_modules/@babel/parser/lib/index.js:786:17)\\n    at Object.jsxParseElementAt (/home/afroz1198/blog/node_modules/@babel/parser/lib/index.js:4726:18)\\n    at Object.jsxParseElement (/home/afroz1198/blog/node_modules/@babel/parser/lib/index.js:4736:17)\\n    at Object.parseExprAtom (/home/afroz1198/blog/node_modules/@babel/parser/lib/index.js:4743:19)\\n    at Object.parseExprSubscripts (/home/afroz1198/blog/node_modules/@babel/parser/lib/index.js:9976:23)\\n    at Object.parseUpdate (/home/afroz1198/blog/node_modules/@babel/parser/lib/index.js:9956:21)\\n    at Object.parseMaybeUnary (/home/afroz1198/blog/node_modules/@babel/parser/lib/index.js:9945:17)\\n    at Object.parseExprOps (/home/afroz1198/blog/node_modules/@babel/parser/lib/index.js:9815:23)\\n    at Object.parseMaybeConditional (/home/afroz1198/blog/node_modules/@babel/parser/lib/index.js:9789:23)\\n    at Object.parseMaybeAssign (/home/afroz1198/blog/node_modules/@babel/parser/lib/index.js:9752:21)\\n    at Object.parseExpressionBase (/home/afroz1198/blog/node_modules/@babel/parser/lib/index.js:9696:23)\\n    at /home/afroz1198/blog/node_modules/@babel/parser/lib/index.js:9690:39\\n    at Object.allowInAnd (/home/afroz1198/blog/node_modules/@babel/parser/lib/index.js:11442:16)\\n    at Object.parseExpression (/home/afroz1198/blog/node_modules/@babel/parser/lib/index.js:9690:17)\");//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiIuL3Bvc3RzL2ZpZnRoLXBvc3QubWR4LmpzIiwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./posts/fifth-post.mdx\n");
+
+/***/ })
+
+})
