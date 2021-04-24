39f49bca4feed54e790f31a0c80d62a68def31f6
Afroz <zkindest5@gmail.com>
2021-04-24 09:16:30 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,12 +4,12 @@ export default {
   subtitle: `Web Developer`,
   description: `I'm Afroz.I build Products with Typescript-React, Node, Rust and Python.`,
   author: `Afroz Mohammad`,
-  userName: `afroz1198`, //default for all
-  mail: `mohammedafroz.1198@gmail.com`,
+  userName: `zkindest`, //default for all
+  mail: `zkindest5@gmail.com`,
   redditUserName: ``,
   twitterUserName: ``,
   twitterId: `2930622986`,
-  baseUrl: `http://afroz1198.github.io`,
+  baseUrl: `http://zkindest.github.io`,
   image: `https://i.imgur.com/8PPjpAD.png`,
   hostedOn: `Github`,
 };
