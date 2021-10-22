a930996396ac97ed06e5894d6d24519653008bdd
zkindest <zkindest5@gmail.com>
2021-10-22 14:15:21 +0530

    Updates

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "48180eadaddd51a00c0e";
+/******/ 	var hotCurrentHash = "1c6dfb7781def675d6bb";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
